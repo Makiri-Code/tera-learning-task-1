cd659350bdd0f3619d6dfc991e3b1f5e9ae9ae83
Emmanuel Amakiri <manuel.makiri@gmail.com>
2024-03-02 16:34:11 +0100

    added showSuccessCard functionality and a onSubit eventListener

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const form = document.getElementById('form');
-const success = document.getElementById('sucess');
-const submitBtn = document.getElementById('submit-btn')
+const success = document.getElementById('success');
+const submitBtn = document.getElementById('submit-btn');
+const formContainer = document.getElementById('form-container');
 
-success.hidden = true
-submitBtn.addEventListener('click', showSuccess)
+const showSuccessCard = (e) => {
+    e.preventDefault();
+    formContainer.hidden = true;
+    success.hidden = false;
+}
+
+form.addEventListener('submit', showSuccessCard)
